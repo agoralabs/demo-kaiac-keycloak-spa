9e9bf6bd143e304a35db46d8d236e8e601ae77af
Joseph Stephane EKOBO KIDOU <agora.labs.contact@gmail.com>
2024-03-22 12:19:29 +0000

    Ecr copy nodejs

--- devops/buildspec.sh
+++ devops/buildspec.sh
@@ -31,3 +31,22 @@ appenvsubstr devops/realm-import.json.template config/realm-import.json
 
 appenvsubstr devops/Dockerfile.template Dockerfile
 appenvsubstr devops/docker-compose.yml.template docker-compose.yml
+
+DOCKER_REGISTRY="$TF_VAR_ENV_APP_GL_DOCKER_REPOSITORY"
+ECR_REGION="$TF_VAR_ENV_APP_GL_AWS_REGION_ECR"
+ECR_REPOSITORY="${TF_VAR_ENV_APP_GL_NAMESPACE}_${TF_VAR_ENV_APP_GL_NAME}_${TF_VAR_ENV_APP_GL_STAGE}"
+ECR_TAG="${TF_VAR_ENV_APP_GL_NAMESPACE}_${TF_VAR_ENV_APP_GL_NAME}_${TF_VAR_ENV_APP_GL_STAGE}"
+
+echo "Building the Docker image..."
+docker build -t $ECR_REPOSITORY:$ECR_TAG .
+
+echo "Create $ECR_REPOSITORY repository..."
+aws ecr describe-repositories --repository-names $ECR_REPOSITORY --region $ECR_REGION || aws ecr create-repository --repository-name $ECR_REPOSITORY --region $ECR_REGION
+aws ecr delete-repository --repository-name $ECR_REPOSITORY --force
+aws ecr create-repository --repository-name $ECR_REPOSITORY --region $ECR_REGION
+
+echo "Tag your image with the Amazon ECR registry..."
+docker tag $ECR_REPOSITORY:$ECR_TAG $DOCKER_REGISTRY/$ECR_REPOSITORY:$ECR_TAG
+
+echo "Push the image to ecr..."
+docker push $DOCKER_REGISTRY/$ECR_REPOSITORY:$ECR_TAG
